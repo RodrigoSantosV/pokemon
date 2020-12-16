f9e85296fa08b701421b50be78e522304b16afeb
Rodrigo Santos <ro.santosvi@gmail.com>
2020-12-16 12:36:04 +0100

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,22 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+
+public class Controller {
+
+    /*@FXML
+    GridPane jolteon;
+    @FXML
+    GridPane charizard;
+    @FXML
+    GridPane vaporeon;
+    @FXML
+    GridPane blastoise;
+    @FXML
+    GridPane mewtwo;
+    @FXML
+    GridPane butterfree;
+    */
+
+}
